f3a4d37c35bec2332532c83f5e7627c362b6b3b0
悦咚 / Dark495 <flandrestudio.cn@gmail.com>
2024-05-25 23:05:09 +0000

    修复音量一大一小的问题；在插件激活时音量条圆点会变红；

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // @name         Youtube真正100%音量
 // @namespace    https://github.com/xlch88/Youtube-Bomb
 // @author       Dark495 (https://dark495.me/)
-// @version      2024-02-03
+// @version      2024-05-26
 // @license      WTFPL
 // @description  Youtube播放器的音量条拖到最大的实际音量值为优化后的结果，并不是100%。用这个脚本可以在你将音量条拖拽到最右侧时给你开到真正百分百的音量。（注：请谨慎使用本脚本，注意用耳健康。Youtube这样做是为了保证每个视频的音量均衡，部分视频可能并不适合使用此脚本的方法暴力提升至100%）
 // @author       Dark495
@@ -16,7 +16,6 @@
 (function() {
     'use strict';
 
-    let needMaxVolume = false;
     let player;
     let volumePanel;
     let volumeSlider;
